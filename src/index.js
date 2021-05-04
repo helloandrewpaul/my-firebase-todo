25432d97c889f86f8c273beb2fa6731595aaff71
“helloandrewpaul” <mcgowanandrew@gmail.com>
2021-05-04 16:10:32 -0400

    add todo components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "./styles.css"
+
 import App from "./App";
 import {} from "./firebase";
 
 const rootDIV = document.getElementById("root");
 
-ReactDOM.render(<App />, rootDIV);
+ReactDOM.render(
+  <>
+    <CssBaseline />
+
+    <App />
+  </>,
+  rootDIV
+);
