c43fdf467cdfb0a4f3739453a526f64ae8143c80
“helloandrewpaul” <mcgowanandrew@gmail.com>
2021-05-04 15:02:57 -0400

    add firebase client

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+// console.log("ginger",firebaseConfig)
+
+firebase.initializeApp(firebaseConfig);
+
+
+export const db = firebase.app().firestore();
